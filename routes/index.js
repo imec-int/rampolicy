8c7e46acba35efecbcdd2fce39013758512ccf71
Matthias De Geyter <matthias.degeyter@ibbt.be>
2014-05-08 13:41:41 +0200

    * removed unnecessary route files * pasted example as comment in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,69 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
+
+
+/*
+
+//OUTPUT{
+	"rules": [{
+		"ID": "<ruleID>",
+		"description": "<description>",
+		"Antecedent": {
+			"ID": "<antecedentID>",
+			"turtle": "<antecedentTurtle>",
+			"description": "<description>",
+
+		},
+		"Consequent": {
+			"ID": "<consequentID>",
+			"turtle": [{"ID":"<ShotID>","SubConsequent":"<turtle>"}],
+			"description": "<description>"
+		}
+	}]
+}
+//INPUT{
+	"rules": [{
+		"ID": "<ruleID>",
+		"AntecedentID": "<antecedentID>",
+		"ConsequentID": "{"ID":"concequentID>","Sequence":["<SubConcequenceID>"]}"  // subconsequenceID
+	}]
+}
+
+//example
+{
+MeetingUser(?user), seatOf(?seat, ?seatNode), unitState(?mic, On), sittingOn(?user, ?seat), unit(?seatNode, ?mic), onSeat(?user, false) -> Show(?user)
+    "rules": [
+        {
+            "Antecedent": {
+                "description": "<description>",
+                "Id": "1",
+                "turtle": televic:MeetingUser(?<urn:swrl#user>), televic:seatOf(?<urn:swrl#seat>,?<urn:swrl#seatNode>),televic:unitState(?<urn:swrl#mic>,televic:On),televic:sittingOn(?<urn:swrl#user>,?<urn:swrl#seat>),televic:unit(?<urn:swrl#seatNode>,?<urn:swrl#mic>)"
+            },
+            "description": "User is speaking",
+            "ID": "1",
+            "Consequent": {
+                "id": "1",
+                "description": "<description>",
+                "turtle": [{"ID":"<ShotID>","SubConsequent":"televic:ShowNationality(?<urn:swrl#user>)"}, {"ID":"<ShotID2>", "SubConsequent":"televic:ShowSpeaker(?<urn:swrl#user>)"}]
+            }
+        },
+        {
+            "Antecedent": {
+                "description": "<description>",
+                "ID": "<antecedentID>",
+                "turtle": "televic:Meeting(?<urn:swrl#meeting>), televic:MeetingUser(?<urn:swrl#user>), televic:attendant(?<urn:swrl#meeting>, ?<urn:swrl#user>), ns:hasAddress(?<urn:swrl#meeting>, ?<urn:swrl#adres>), ns:hasAddress(?<urn:swrl#user>, ?<urn:swrl#adres>)"
+            },
+            "description": "<Meeting and user have the same location>",
+            "ID": "2",
+            "Consequent": {
+                "description": "<description>",
+                "ID": "<concequentID>",
+                "turtle": [{"ID":"<ShotID>","SubConsequent":"televic:ShowNationality(?<urn:swrl#user>)"},{"ID":"<ShotID2>", "SubConsequent":"televic:ShowSpeaker(?<urn:swrl#user>)"}]
+            }
+        }
+    ]
+
+}
+
+*/
